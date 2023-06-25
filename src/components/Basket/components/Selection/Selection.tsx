2c42fe7fd5688ba238e5b5d4f71e68f1bd14b050
Wesley Bersi <wesleybersi@icloud.com>
2023-06-25 16:40:26 +0200

    Stupid bug

--- src/components/Basket/components/Selection/Selection.tsx
+++ src/components/Basket/components/Selection/Selection.tsx
@@ -11,6 +11,7 @@ const Selection: React.FC<Props> = ({ type, item }) => {
   const { parameters, method } = useStore();
   const selectionRef = useRef<HTMLDivElement | null>(null);
   const [color, setColor] = useState<string>("");
+  const [emoji, setEmoji] = useState<string>("");
 
   useEffect(() => {
     if (type === "red") {
@@ -24,43 +25,36 @@ const Selection: React.FC<Props> = ({ type, item }) => {
     }
   }, [type]);
 
-  useCSSProperty(selectionRef.current, "--selectionColor", color);
-  // if (type === "target") return <span className="basket-item-target" />;
+  useEffect(() => {
+    if (method.title !== "with") return;
+    const param = parameters.get(1);
+    const value = param?.value;
+    if (value && value instanceof Emoji) {
+      setEmoji(value.emoji);
+    }
+  }, [parameters.get(1)?.value]);
 
-  if (type === "highlight")
-    return (
-      <span
-        // style={{ backgroundColor: "var(--highlight)" }}
-        className="basket-item-selected"
-      />
-    );
+  useEffect(() => {
+    if (method.title !== "fill") return;
+    const param = parameters.get(0);
+    const value = param?.value;
+    if (value && value instanceof Emoji) {
+      setEmoji(value.emoji);
+    }
+  }, [parameters.get(0)?.value]);
+
+  useEffect(() => {
+    if (method.title !== "with" && method.title !== "fill") {
+      setEmoji(item);
+    }
+  }, [method.title]);
+
+  useCSSProperty(selectionRef.current, "--selectionColor", color);
 
   return (
-    <>
-      <div
-        ref={selectionRef}
-        style={
-          {
-            // border:
-            //   type === "blue"
-            //     ? "3px dashed var(--selection)"
-            //     : "3px dashed var(--deleteCount)",
-            // backgroundColor:
-            //   type === "blue"
-            //     ? "var(--selectionOpaque)"
-            //     : "var(--deleteCountOpaque)",
-          }
-        }
-        className="basket-item-selected"
-      >
-        {method.title === "with" &&
-          parameters.get(1)?.value instanceof Emoji &&
-          parameters.get(1)?.value?.emoji.toString()}
-        {method.title === "fill" && parameters.get(0)?.value?.emoji.toString()}
-        {method.title !== "with" && method.title !== "fill" && item}
-      </div>
-      <span className="basket-item-selected-blend" />
-    </>
+    <div ref={selectionRef} className="basket-item-selected">
+      {emoji}
+    </div>
   );
 };
 
